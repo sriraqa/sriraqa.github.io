4c3baf077573867778a78ca91bb6f0fe2636c300
Sarah Q <qiaosarah8@gmail.com>
2025-06-12 23:02:51 -0400

    new project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ import Button from "../components/Button";
 import sparkles from "../assets/sparkles.png";
 import illustration from "../assets/illustration.png";
 import illustrationActive from "../assets/illustration-active.png";
+import scrapcard from "../assets/scrapcard.png";
 import navcare from "../assets/navcare.png";
 import interac from "../assets/interac.png";
 import newsbear from "../assets/newsbear.png";
@@ -81,6 +82,46 @@ export default function Home() {
         id="portfolio"
         className="flex flex-col space-y-12 w-full max-w-[812px] pt-24"
       >
+        <Button>
+          <NavHashLink to="/interac#top" className="menu-item">
+            <div className="flex flex-row space-x-6 items-center">
+              <div className="sm:w-3/5 flex flex-col space-y-4">
+                <div className="flex flex-col">
+                  <h2 className="text-[32px] font-semibold">Scrap Card</h2>
+                  <p className="text-body/60 font-medium text-[16px]">
+                    Current Personal Project
+                  </p>
+                </div>
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0"
+                    y="0"
+                    width="100%"
+                    height="7"
+                    fill="none"
+                  >
+                    <path
+                      d="M1 1.443C1 2.62951 9.46221 2.93638 11.78 3.00711C17.7839 3.19034 22.5205 1.66631 28.3678 1.30178C34.6693 0.908937 41.2777 0.90344 47.5801 1.26304C61.1552 2.0376 72.817 5.40653 86.6614 5.40653C94.0403 5.40653 100.192 3.94417 107.22 3.23356C114.035 2.54454 121.24 2.30799 128.216 2.10134C134.023 1.9293 140.041 1.8355 145.765 2.31478C152.264 2.85887 157.952 4.3334 164.188 5.20808C170.033 6.02785 177.046 6.15322 183.145 5.84767C190.172 5.49564 195.551 4.04813 202.096 3.14583C215.163 1.34432 228.874 0.285019 242.328 1.95262C251.72 3.11676 260.041 4.69472 269.95 5.06561C280.891 5.47513 291.036 4.05845 301 2.28879"
+                      stroke="#46464A"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+                <h6 className="text-[16px]">
+                  Mobile app project that I am building from scratch! Helping
+                  people reflect and maintain meaningful relationships online
+                </h6>
+                <p className="text-primary text-[16px]">In Progress 🔒</p>
+              </div>
+              <div className="w-2/5 hidden sm:block">
+                <img src={scrapcard} alt="Scrap Card Project" />
+              </div>
+            </div>
+          </NavHashLink>
+        </Button>
         <Button>
           <NavHashLink to="/interac#top" className="menu-item">
             <div className="flex flex-row space-x-6 items-center">
